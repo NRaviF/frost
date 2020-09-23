59f94902fedfbcece9441845398e50f8179ccf63
NRaviF <nravi.f@gmail.com>
2020-09-23 20:15:07 +0700

    Biodata

---
+++ mainbiodata.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	printf("====================Biodata====================\n");
+	printf("Nama\t: Noor Ravi Fitradhi\n");
+	printf("NPM\t: 19081010115\n");
+	printf("TTL\t: Surabaya, 27 Mei 2001\n");
+	printf("Asal\t: Surabaya\n");
+	printf("Alamat\t: Jl. Simomulyo Baru 6E/11, Simomulyo Baru, Sukomanunggal, Surabaya\n");
+	printf("No. Hp\t: 082233677668\n");
+	printf("===============================================");
+	return 0;
+}
